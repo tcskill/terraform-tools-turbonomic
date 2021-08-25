7c5bdacc94b01bcd144d49dc78aa00bf31d25ea6
Thomas Skill <tskill@us.ibm.com>
2021-08-25 15:57:06 -0500

    pwdchange

--- scripts/createStorageClass.sh
+++ scripts/createStorageClass.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
+MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
 
-kubectl create -f "${SCRIPT_DIR}/storageclass-ibmc-vpc-block-10iops-mzr.yaml"
+kubectl create -f "${MODULE_DIR}/storageclass-ibmc-vpc-block-10iops-mzr.yaml"
