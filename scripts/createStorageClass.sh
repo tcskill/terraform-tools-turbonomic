af55ddc3eb58927da0793a938c882cb6a362a482
Thomas Skill <tskill@us.ibm.com>
2021-08-25 16:15:00 -0500

    dirfix

--- scripts/createStorageClass.sh
+++ scripts/createStorageClass.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
-MODULE_DIR=$(cd "${SCRIPT_DIR}/.."; pwd -P)
+MODULE_DIR=$(cd ".."; pwd -P)
 
-kubectl create -f "${MODULE_DIR}/storageclass-ibmc-vpc-block-10iops-mzr.yaml"
+kubectl create -f "${MODULE_DIR}/scripts/storageclass-ibmc-vpc-block-10iops-mzr.yaml"
