ec9e6feda883cff80cc3a8b75dd6c73ddc6d6ef3
Thomas Skill <tskill@us.ibm.com>
2021-08-26 09:50:58 -0500

    chrtmv

--- scripts/configStorageClass.sh
+++ scripts/configStorageClass.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-SCRIPT_DIR=$(cd $(dirname $0); pwd -P)
+
+CHARTS_DIR=$(cd $(dirname $0)/../charts; pwd -P)
 
 if [[ "$1" == "destroy" ]]; then
     echo "removing storage class..."
-    kubectl delete -f "${SCRIPT_DIR}/storageclass-ibmc-vpc-block-10iops-mzr.yaml"
+    kubectl delete -f "${CHARTS_DIR}/storageclass-ibmc-vpc-block-10iops-mzr.yaml"
 else 
-    kubectl create -f "${SCRIPT_DIR}/storageclass-ibmc-vpc-block-10iops-mzr.yaml"
+    kubectl create -f "${CHARTS_DIR}/storageclass-ibmc-vpc-block-10iops-mzr.yaml"
 fi
