1a4a73ef90a26cd524143602fa70f22f21ec8deb
Thomas Skill <tskill@us.ibm.com>
2021-08-30 13:46:16 -0500

    opfx

--- scripts/installOperator.sh
+++ scripts/installOperator.sh
@@ -12,8 +12,8 @@ CHARTS_DIR=$(cd $(dirname $0)/../charts; pwd -P)
 if [[ "$3" == "destroy" ]]; then
     echo "removing chart extension..."
     # remove the the operator and chart extensions
-    kubectl delete -f "${TMP_DIR}/operator.yaml" -n ${NAMESPACE}
-    kubectl delete -f "${CHARTS_DIR}/charts_v1alpha1_xl_crd.yaml"
+    kubectl delete -f "${TMP_DIR}/operator.yaml" -n ${NAMESPACE} --validate=false
+    kubectl delete -f "${CHARTS_DIR}/charts_v1alpha1_xl_crd.yaml" 
 else 
     # deploy the chart extensions needed
     kubectl create -f "${CHARTS_DIR}/charts_v1alpha1_xl_crd.yaml"
@@ -68,7 +68,7 @@ template:
     - name: operator-tmpfs0
         emptyDir: {}
 EOL
-    kubectl create -f "${TMP_DIR}/operator.yaml" -n ${NAMESPACE}
+    kubectl create -f "${TMP_DIR}/operator.yaml" -n ${NAMESPACE} --validate=false
 fi
 
 
