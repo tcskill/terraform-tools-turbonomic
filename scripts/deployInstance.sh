59bfdc8a38a38120b474c4424467acca9dac6245
Thomas Skill <tskill@us.ibm.com>
2021-09-01 07:41:39 -0500

    ctfx

--- scripts/deployInstance.sh
+++ scripts/deployInstance.sh
@@ -15,23 +15,23 @@ if [[ "$3" == "destroy" ]]; then
     kubectl delete -f "${TMP_DIR}/xl-release.yaml" -n ${NAMESPACE}
 else 
     echo "copying... ${CHARTS_DIR}/t8c-xl-release-mzr.yaml ${TMP_DIR}/xl-release.yaml"
-    cp ${CHARTS_DIR}/t8c-xl-release-mzr.yaml ${TMP_DIR}/xl-release.yaml
+    cp "${CHARTS_DIR}/t8c-xl-release-mzr.yaml" "${TMP_DIR}/xl-release.yaml"
     if [[ "${PROBES}" =~ kubeturbo ]]; then
-      cat ${TMP_DIR}/xl-release.yaml > "${TMP_DIR}/xl-release.yaml" << EOL
+      cat ${TMP_DIR}/xl-release.yaml << EOL
   kubeturbo:
     enabled: true
 EOL
     fi
 
     if [[ "${PROBES}" =~ openshiftingress ]]; then
-      cat ${TMP_DIR}/xl-release.yaml > "${TMP_DIR}/xl-release.yaml" << EOL
+      cat ${TMP_DIR}/xl-release.yaml << EOL
   openshiftingress:
     enabled: true
 EOL
     fi
 
     if [[ "${PROBES}" =~ instana ]]; then
-      cat ${TMP_DIR}/xl-release.yaml > "${TMP_DIR}/xl-release.yaml" << EOL
+      cat ${TMP_DIR}/xl-release.yaml << EOL
   instana:
     enabled: true
 EOL
