345db4cf61f36245cf85933a8fef43c38744523a
Thomas Skill <tskill@us.ibm.com>
2021-09-09 16:35:05 -0500

    addsleep

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -10,13 +10,10 @@ fi
 export KUBECONFIG
 
 #wait for pods to start
-wait 15s
+sleep 5m
 
-NAMESPACE=$(echo "var.turbo_namespace" | terraform console -var-file variables.tf)
-
-
-kubectl rollout status deployment/rsyslog -n ${NAMESPACE}
-if [[ "$?" -ne 0 ]] then
-    echo "turbo deployment failed in namespace: ${NAMESPACE} exit code $?"
+kubectl rollout status deployment/rsyslog -n
+if [[ "$?" -ne 0 ]]; then
+    echo "turbo deployment failed with exit code $?"
     exit 1
 fi
