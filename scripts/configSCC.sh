75cdc3ec4ffd245d226b940ad0f1d708bb30f6be
Thomas Skill <tskill@us.ibm.com>
2021-09-22 17:20:31 -0500

    sccupdate

--- scripts/configSCC.sh
+++ scripts/configSCC.sh
@@ -15,5 +15,5 @@ if [[ "$4" == "destroy" ]]; then
     echo "removing turbo scc..."
     ${BIN_DIR}/helm template t8scc_chart service-account --repo https://charts.cloudnativetoolkit.dev --set "name=${SANAME}" --set sccs[0]=anyuid --set create=false --set "-n=${NAMESPACE}" | kubectl delete -n "${NAMESPACE}" -f -
 else 
-    ${BIN_DIR}/helm template t8scc_chart service-account --repo https://charts.cloudnativetoolkit.dev --set "name=${SANAME}" --set sccs[0]=anyuid --set create=false --set "-n=${NAMESPACE}" | kubectl apply -n "${NAMESPACE}" -f -
+    ${BIN_DIR}/helm template t8scc_chart service-account --repo https://charts.cloudnativetoolkit.dev --set "name=${SANAME}" --set sccs[0]=anyuid --set create=true --set "-n=${NAMESPACE}" | kubectl apply -n "${NAMESPACE}" -f -
 fi
