fede8f1e734acac9bbebbf2946ffea81a994f9d7
Thomas Skill <tskill@us.ibm.com>
2021-09-22 18:31:55 -0500

    opnamechg

--- variables.tf
+++ variables.tf
@@ -23,7 +23,7 @@ variable "tls_secret_name" {
 variable "turbo_service_account_name" {
   description = "The name of the service account that should be used for the deployment"
   type        = string
-  default     = "turbo-user"
+  default     = "t8c-operator"
 }
 
 variable "turbo_probes" {
